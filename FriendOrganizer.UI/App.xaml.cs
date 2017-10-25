398c7bd86bf8b6dc1318cd958c69de36e7ce4fb5
Daniel-sann <mista.dan@hotmail.com>
2017-10-25 21:59:55 +0200

    Added Autofac

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Startup;
+using FriendOrganizer.UI.ViewModel;
+using Autofac;
 
 namespace FriendOrganizer.UI
 {
@@ -13,5 +18,15 @@ namespace FriendOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
+
+       
     }
 }
