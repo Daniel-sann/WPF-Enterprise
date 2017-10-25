d15eaf6b713c41585f16f2e3ffd514f353aecba9
Daniel-sann <mista.dan@hotmail.com>
2017-10-25 16:50:56 +0200

    Added MainViewModel

---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IFriendDataService _friendDataService;
+        private Friend _selectedFriend;
+
+        
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+        public ObservableCollection<Friend> Friends { get; set; }
+
+
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+            }
+        }  
+    }
+}
