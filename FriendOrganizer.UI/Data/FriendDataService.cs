ee2d97a887e920fb5c1a53bbd0cbbb326bcc0ad9
Daniel-sann <mista.dan@hotmail.com>
2017-10-25 16:30:21 +0200

    Added data folder and dataservice

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend {FirstName = "Mike", LastName = "Hawk"};
+            yield return new Friend { FirstName = "Ben", LastName = "Dover" };
+            yield return new Friend { FirstName = "Isaac", LastName = "Cox" };
+            yield return new Friend { FirstName = "Mike", LastName = "Hunt" };
+        }
+    }
+}
