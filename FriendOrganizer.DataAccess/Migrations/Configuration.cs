6fc14a31893f1b8bef36a935590e2b94d2043ce7
Daniel-sann <mista.dan@hotmail.com>
2017-10-26 16:23:44 +0200

    Added migrations

---
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(
+                f => f.FirstName,
+                new Friend { FirstName = "Mike", LastName = "Hawk" },
+                new Friend { FirstName = "Ben", LastName = "Dover" },
+                new Friend { FirstName = "Isaac", LastName = "Cox" },
+                new Friend { FirstName = "Mike", LastName = "Hunt" }
+            );
+        }
+    }
+}
