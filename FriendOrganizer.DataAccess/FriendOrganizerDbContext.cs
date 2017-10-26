64f6a70b6946b15939d611c01bfd942c667145b2
Daniel-sann <mista.dan@hotmail.com>
2017-10-26 13:42:17 +0200

    Added entity framework and datacontext class

---
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.DataAccess
+{
+    public class FriendOrganizerDbContext : DbContext
+    {
+        public FriendOrganizerDbContext():base("FriendOrganizerDb")
+        {
+            
+        }
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
