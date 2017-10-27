0c0ab46bc6bafca9d2f1f3c18f8e0d52b4516de1
Daniel-sann <mista.dan@hotmail.com>
2017-10-27 12:52:57 +0200

    Added prism and eventhandler

--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -2,16 +2,20 @@
 using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private readonly IFriendLookupDataService _friendLookupDataService;
+        private readonly IEventAggregator _eventAggregator;
 
-        public NavigationViewModel(IFriendLookupDataService friendLookupDataService)
+        public NavigationViewModel(IFriendLookupDataService friendLookupDataService, IEventAggregator eventAggregator)
         {
             _friendLookupDataService = friendLookupDataService;
+            _eventAggregator = eventAggregator;
             Friends = new ObservableCollection<LookupItem>();
         }
 
@@ -24,7 +28,22 @@ namespace FriendOrganizer.UI.ViewModel
                 Friends.Add(item);
             }
         }
-
         public ObservableCollection<LookupItem> Friends { get; }
+        private LookupItem _selectedFriend;
+
+        public LookupItem SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                {
+                    _eventAggregator.GetEvent<OpenFriendDetailViewEvent>().Publish(_selectedFriend.Id);
+                }
+            }
+        }
+
     }
 }
