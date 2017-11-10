3e711bbe7a658eb8120b45313c1a186fe717ae8b
Daniel-sann <mista.dan@hotmail.com>
2017-11-10 13:05:45 +0100

    Added detailmeetingviewmodel

---
+++ FriendOrganizer.UI/Wrapper/MeetingWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class MeetingWrapper : ModelWrapper<Meeting>
+    {
+        public MeetingWrapper(Meeting model) : base(model)
+        {
+            
+        }
+
+        public int Id { get; set; }
+
+        public string Title
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value);}
+        }
+
+        public DateTime DateFrom
+        {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value);}
+        }
+
+        public DateTime DateTo
+        {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value);}
+        }
+    }
+}
